f872dd0aa3a09adc6d254dfa5927e6c599437c60
Evan Parker <evanparker@Evans-MacBook-Pro.local>
2019-02-06 19:44:53 -0500

    Initial commit

---
+++ Colorblind.R
@@ -0,0 +1,11 @@
+# Color palette for the colorblind
+# Source: http://jfly.iam.u-tokyo.ac.jp/html/manuals/pdf/color_blind.pdf
+
+pa_blue <- "#0072B2"          # RGB{0,114,178}
+pa_green <- "#009E73"         # RGB{0,158,115}
+pa_orange <- "#D55E00"        # RGB{230,159,0}
+pa_lightblue <- "#56B4E9"     # RGB{86,180,233}
+pa_pink <- "#CC79A7"          # RGB{204,121,167}
+pa_lightorange <- "#E69F00"   # RGB{230,159,0}
+pa_yellow <- "#F0E422"        # RGB{240,228,66}
+
